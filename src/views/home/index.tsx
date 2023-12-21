10e0ee4ed293910bbd9846490cb8a6a31c287fca
EthanOK <sdzdj01@163.com>
2023-12-21 17:42:19 +0800

    new file:   .env

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -7,6 +7,9 @@ import { useWallet, useConnection } from '@solana/wallet-adapter-react';
 
 // Components
 import { RequestAirdrop } from '../../components/RequestAirdrop';
+
+import { CandyMint } from '../../components/CandyMint';
+
 import pkg from '../../../package.json';
 
 // Store
@@ -50,6 +53,7 @@ export const HomeView: FC = ({ }) => {
         </div>
         <div className="flex flex-col mt-2">
           <RequestAirdrop />
+          <CandyMint /> 
           <h4 className="md:w-full text-2xl text-slate-300 my-2">
           {wallet &&
           <div className="flex flex-row justify-center">
